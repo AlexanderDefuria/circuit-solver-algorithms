ac60dd2e7e83aad69d327c2a74a36c95a6d5f13b
AlexanderDeFuria <defuria.alexander@gmail.com>
2023-05-24 21:06:33 -0400

    initial building of nodes

--- src/container.rs
+++ src/container.rs
@@ -1,10 +1,11 @@
 use crate::components::Component;
 use crate::components::Component::Ground;
+use crate::container::SolutionState::Partial;
 use crate::elements::Element;
-use crate::tools::Tool;
-use crate::validation::StatusError::KnownIssue;
+use crate::tools::{Tool, ToolType};
+use crate::validation::StatusError::Known;
 use crate::validation::{
-    Validation, check_duplicates, get_all_internal_status_errors, Status, StatusError,
+    check_duplicates, get_all_internal_status_errors, Status, StatusError, Validation,
     ValidationResult,
 };
 use serde::{Deserialize, Serialize};
@@ -83,9 +84,14 @@ impl<'a> Container<'a> {
         id
     }
 
-    fn add_tool<'tool: 'a>(&mut self, tool: Tool<'tool>) {
-        // TODO
-        self.tools.push(tool);
+    fn add_tool<'tool: 'a>(mut tool: Tool<'tool>, tool_list: &mut  Vec<Tool<'tool>>) {
+        if !tool_list.is_empty() {
+            let new_id: usize = tool_list.get(tool_list.len() - 1).unwrap().id + 1;
+            tool.id = new_id;
+        } else {
+            tool.id = 0;
+        }
+        tool_list.push(tool);
     }
 
     /// Manually set the ground element
@@ -125,8 +131,44 @@ impl<'a> Container<'a> {
         self.validate()
     }
 
+    /// Create the Nodes and add them to the Container Tools
+    ///
+    /// This process can be done by sampling one side of every element and then
+    /// comparing the samples to see if they are the same. If they are the same
+    /// then they are connected and should be added to the same node.
+    /// By by filtering our duplicates we can create a pure list of nodes.
+    pub fn create_nodes(&'a mut self) -> &'a Self {
+        for element in &self.elements {
+            let mut positive_elements: Vec<&Element> = element
+                .positive
+                .iter()
+                .filter_map(|positive_id: &usize| {
+                    self.elements
+                        .iter()
+                        .find(|container_element| container_element.id == *positive_id)
+                })
+                .collect();
+
+            positive_elements.push(element);
+
+            if !self.tools.iter().any(|x| {
+                // Only care about nodes
+                if x.pseudo_type == ToolType::Node {
+                    x.elements
+                        .iter()
+                        .all(|tool_elements| positive_elements.contains(tool_elements))
+                } else {
+                    false
+                }
+            }) {
+                Container::add_tool(Tool::create_node(positive_elements), &mut self.tools);
+            }
+        }
+
+        self
+    }
+
     pub fn create_mesh(&mut self) {}
-    pub fn create_nodes(&mut self) {}
     pub fn create_super_mesh(&mut self) {}
     pub fn create_super_nodes(&mut self) {}
     pub fn create_thevenin(&mut self) {}
@@ -147,16 +189,16 @@ impl Validation for Container<'_> {
 
         // Check that there is at least one source and a single ground
         if !self.elements.iter().any(|x| x.class.is_source()) {
-            errors.push(KnownIssue("No Sources".parse().unwrap()));
+            errors.push(Known("No Sources".parse().unwrap()));
         }
         if self.elements.iter().filter(|x| x.class == Ground).count() != 1 {
-            errors.push(KnownIssue("Multiple Grounds".parse().unwrap()));
+            errors.push(Known("Multiple Grounds".parse().unwrap()));
         }
 
         match errors.len() {
             0 => Ok(Status::Valid),
             1 => Err(errors[0].clone()),
-            _ => Err(StatusError::MultipleIssues(errors)),
+            _ => Err(StatusError::Multiple(errors)),
         }
     }
 }
@@ -168,14 +210,16 @@ mod tests {
     use crate::elements::Element;
     use crate::validation::{Status, StatusError, Validation};
 
+    use crate::tools::ToolType::Node;
     use regex::Regex;
+    use crate::validation::Status::Valid;
 
     fn create_basic_container<'a>() -> Container<'a> {
         let mut container = Container::new();
-        container.add_element_core(Element::new(Resistor, 1.0, vec![2], vec![3]));
-        container.add_element_core(Element::new(Resistor, 1.0, vec![2], vec![3]));
-        container.add_element_core(Element::new(Ground, 1.0, vec![2], vec![]));
-        container.add_element_core(Element::new(VoltageSrc, 1.0, vec![2], vec![3]));
+        container.add_element_core(Element::new(VoltageSrc, 1.0, vec![2, 3], vec![1]));
+        container.add_element_core(Element::new(Resistor, 1.0, vec![0], vec![2]));
+        container.add_element_core(Element::new(Resistor, 1.0, vec![1], vec![0, 3]));
+        container.add_element_core(Element::new(Ground, 1.0, vec![0, 2], vec![]));
         container
     }
 
@@ -192,7 +236,7 @@ mod tests {
     #[test]
     fn test_validate() {
         let mut container = create_basic_container();
-        assert_eq!(container.validate().unwrap(), Status::Valid);
+        assert_eq!(container.validate(), Ok(Status::Valid));
 
         // Test duplicate elements, this should be invalid.
         container.elements[0].id = 1;
@@ -218,5 +262,16 @@ mod tests {
         let result: Result<usize, StatusError> =
             container.add_element(Element::new(Ground, 1.0, vec![2], vec![]));
         assert!(result.is_err());
+    }
+
+    #[test]
+    fn test_create_nodes() {
+        let mut container = create_basic_container();
+        let x = container.create_nodes();
+        assert_eq!(x.validate(), Ok(Valid));
+        assert_eq!(x.tools.len(), 3);
+        assert_eq!(x.tools[0].elements, vec![&x.elements[2], &x.elements[3], &x.elements[0]]);
+        assert_eq!(x.tools[1].elements, vec![&x.elements[0], &x.elements[1]]);
+        assert_eq!(x.tools[2].elements, vec![&x.elements[1], &x.elements[2]]);
     }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -3,21 +3,7 @@ use wasm_bindgen::prelude::*;
 pub mod components;
 pub mod container;
 pub mod elements;
+pub mod intermediaries;
+mod tests;
 pub mod tools;
 pub mod validation;
-mod tests;
-
-#[wasm_bindgen]
-pub fn solve() -> String {
-    String::from("Hello, world!")
-}
-
-#[wasm_bindgen]
-pub fn add(x: i32, y: i32) -> i32 {
-    x + y
-}
-
-pub fn simplify() {}
-
-pub fn serialize() {}
-pub fn deserialize() {}
